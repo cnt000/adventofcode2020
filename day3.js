a326b2a06ebbe7f6ff81a7898b7e7f319c388e8b
Edoardo Gargano <edoardo.gargano@monrif.net>
2020-12-11 16:39:01 +0100

    fghfgh

--- day3.js
+++ day3.js
@@ -1,16 +1,49 @@
 const { input } = require('./input/day3');
-const inputList = input.split('\n');
-const start = [0, 0];
+
+// const inputList = input.split('\n');
+// const xLength = inputList[0].length;
+// const yLength = inputList.length;
+// const start = [0, 0];
 let result = 0;
-const move = (x, y) => [x + 3, y + 1];
-console.log(inputList[start[0]][start[1]], start);
-let movedCoord = move(...start);
-console.log(inputList[movedCoord[0]][movedCoord[1]]);
-for (let i = 0; i < inputList[0].length; i++) {
+// const move = (x, y) => [x + 3, y + 1];
+// let movedCoord = start;
+
+
+
+// console.log(inputList[start[0]][start[1]], start);
+// console.log(inputList[movedCoord[0]][movedCoord[1]]);
+
+// for (let i = 0; i < inputList.length; i++) {
+
+//   movedCoord = move(...movedCoord);
+//   if (inputList[movedCoord[0]][movedCoord[1]] === '#') {
+//     result++;
+//   }
+//   console.log(inputList[movedCoord[0]][movedCoord[1]]);
+// }
+
+let x = 0;
+let y = 0;
+while(true) {
+
+  if (y >= input.length) break;
+
   movedCoord = move(...movedCoord);
-  if (inputList[movedCoord[0]][movedCoord[1]] === '#') {
+  // if(movedCoord[0] >= xLength) movedCoord[0] = 0;
+  // console.log(
+  //   y,
+  //   inputList[y],
+  //   movedCoord[0],
+  //   movedCoord[1],
+  //   inputList[movedCoord[1]][movedCoord[0]],
+  // );
+  const pos = input.charAt(y % input.length);
+  console.log(pos);
+
+  if (pos === '#') {
     result++;
   }
-  console.log(inputList[movedCoord[0]][movedCoord[1]]);
+  y++;
 }
+
 console.log(result);
