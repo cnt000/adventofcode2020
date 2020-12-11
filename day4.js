c57f7130d89a0d0fc56c00c13b1f3a45d259dc9c
Edoardo Gargano (cnt000) <cnt000@gmail.com>
2020-12-11 22:53:13 +0100

    day 4 part 1

---
+++ day4.js
@@ -0,0 +1,28 @@
+const { input } = require('./input/day4');
+
+const passports = input.split('\n\n');
+const regex = (field) => `(.*?)[ \n]*${field}:(.*?)`;
+
+const regexbyr = new RegExp(regex('byr'), 'm');
+const regexiyr = new RegExp(regex('iyr'), 'm');
+const regexeyr = new RegExp(regex('eyr'), 'm');
+const regexhgt = new RegExp(regex('hgt'), 'm');
+const regexhcl = new RegExp(regex('hcl'), 'm');
+const regexecl = new RegExp(regex('ecl'), 'm');
+const regexpid = new RegExp(regex('pid'), 'm');
+
+const isValid = (passport) => !!passport.match(regexbyr) &&
+  !!passport.match(regexiyr) &&
+  !!passport.match(regexeyr) &&
+  !!passport.match(regexhgt) &&
+  !!passport.match(regexhcl) &&
+  !!passport.match(regexecl) &&
+  !!passport.match(regexpid);
+
+let result = 0;
+for (let i = 0; i < passports.length; i++) {
+  if(isValid(passports[i])) {
+    result++
+  }
+}
+console.log(result);
