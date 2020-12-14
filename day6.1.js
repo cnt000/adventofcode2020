79db28e9aa1215904028983eba89c1023d17298c
Edoardo Gargano <edoardo.gargano@monrif.net>
2020-12-14 10:48:03 +0100

    day 6 part 2, to refactor

--- day6.1.js
+++ day6.1.js
@@ -2,16 +2,35 @@ const { input } = require('./input/day6');
 const assert = require('assert').strict;
 
 const countPositiveAnswers = (answers) => {
-  return answers
+  let res = 0;
+  answers
     .split('\n\n')
-    .map(group => group
-      .split('\n')
-      //.reduce((acc, curr) => [...curr, ...acc], acc), [])
-    );
-}
+    .map((group) => getIntersectionLetters(group.split('\n')))
+    .forEach(letters => res += letters.length);
+  return res;
+};
+
+const getAlphabetPosition = (char) => char.charCodeAt(0) - 97;
+
+const getIntersectionLetters = (words) => {
+  let res = [];
+  const max = words.length;
+  let letters = Array(26).fill(0);
+  words.forEach((word) =>
+    word.split('').forEach((char) => {
+      letters[getAlphabetPosition(char)] = letters[getAlphabetPosition(char)] + 1;
+      if (letters[getAlphabetPosition(char)] >= max) {
+        res.push(char);
+      }
+    }),
+  );
+
+  return Array.from(new Set(res)).join('');
+};
 
 const test = () => {
-  assert.deepStrictEqual(countPositiveAnswers(`abc
+  assert.deepStrictEqual(
+    countPositiveAnswers(`abc
 
 a
 b
@@ -25,8 +44,23 @@ a
 a
 a
 
-b`), 6);
-}
+b`),
+    6,
+  );
+};
 
-// test();
+const testAlphabetPosition = () => {
+  assert.deepStrictEqual(
+    getIntersectionLetters([
+      'abc',
+      'ab',
+      'ca',
+    ]),
+    'a',
+  );
+};
+
+test();
+testAlphabetPosition();
+
 console.log(countPositiveAnswers(input));
