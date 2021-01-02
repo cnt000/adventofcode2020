16135de45e753cabcbeaf4e7fc5032b96f8e8300
Edoardo Gargano (cnt000) <cnt000@gmail.com>
2021-01-02 17:21:50 +0100

    finally 8.1

--- day8.1.js
+++ day8.1.js
@@ -11,35 +11,87 @@ acc +1
 jmp -4
 acc +6`.split('\n');
 
-const exampleNoLoop = `nop +0
-acc +1
-jmp +4
-acc +3
-jmp -3
-acc -99
-acc +1
-nop -4
-acc +6`.split('\n');
+// const exampleNoLoop = `nop +0
+// acc +1
+// jmp +4
+// acc +3
+// jmp -3
+// acc -99
+// acc +1
+// nop -4
+// acc +6`.split('\n');
 
-let path = [];
-console.log('Result:', exec(exampleLoop));
-path = [];
-console.log('Result:', exec(exampleNoLoop));
+console.log(find(splittedLines));
+// path = [];
+// find(exampleNoLoop);
+// path = [];
+// find(splittedLines);
 
-function exec(input) {
+function find(input) {
   let position = 0;
-  while (true) {
-    let incr = getNextPos(input[position]);
-    console.log(
-      input[position],
-      position % input.length,
-    );
-    position += incr;
-    if (path.includes(position)) return 'Loop!'
-    path.push(position);
-    if (position === input.length) break;
+  let acc = 0;
+  const originalInput = [...input];
+  const [correctOrLoop, counter] = isCorrect(originalInput, acc);
+  if (correctOrLoop) {
+    console.log('correct', counter);
+    return;
+  } else {
+    console.log('loop', counter);
   }
-  return 'Finish!';
+  while (true) {
+    // sostituisci la prima occorrenza di jmp o nop e salva la posizione di sostituzione
+    const [inputFixed, substitutionPosition] = subsFirst(originalInput, position);
+    position = substitutionPosition + 1;
+    console.log(inputFixed);
+    // prova con questo input, controlla che arriva in fondo, se si, finisce e torna counter, altrimenti cicla
+    const [correctOrLoop, newAcc] = isCorrect([...inputFixed], acc);
+  //   acc += counter;
+    // console.log(correctOrLoop, substitutionPosition, newAcc, [...inputFixed]);
+    if (correctOrLoop) { return newAcc; }
+  }
+}
+
+// a jmp is supposed to be a nop, or a nop is supposed to be a jmp
+function subsFirst(input, start) {
+  let foundIndex = 0;
+  let modifiedInput = [...input];
+  for (let i = start; i < input.length; i++) {
+    const matches = input[i].match(/(jmp|nop) ([+|-]{1}[0-9]+)/);
+    if (matches) {
+      const [, instruction, value] = matches;
+      if (instruction === 'nop') {
+        modifiedInput[i] = `jmp ${value}`;
+        foundIndex = i;
+        break;
+      }
+      if (instruction === 'jmp') {
+        modifiedInput[i] = `nop ${value}`;
+        foundIndex = i;
+        break;
+      }
+    }
+  }
+  // console.log(input, foundIndex);
+  return [modifiedInput, foundIndex]
+}
+
+function isCorrect(input, acc) {
+  let position = 0;
+  let path = [];
+  while (true) {
+    let [positionIncr, counterIncr] = getNextPos(input[position]);
+    position += positionIncr;
+    acc += counterIncr;
+    console.log(
+      position,
+      input.length
+    );
+    if (position === input.length) {
+      return [true, acc];
+    }
+    if (path.includes(position)) return [false, acc];
+    path.push(position);
+  }
 }
 
 function getNextPos(cmd) {
@@ -47,5 +99,6 @@ function getNextPos(cmd) {
   const [, op, opValue] = cmd.match(regex);
   const opNumber = Number(opValue);
   const positionIncr = op === 'jmp' ? opNumber : 1;
-  return positionIncr;
+  const counterIncr = op === 'acc' ? opNumber : 0;
+  return [positionIncr, counterIncr];
 }
